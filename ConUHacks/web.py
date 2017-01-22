9eeb2ae84967c6e069a7778ddc9bd2949653e78f
Fondson Tran <fondsont@gmail.com>
2017-01-21 20:37:53 -0500

    don't mind this commit

--- ConUHacks/web.py
+++ ConUHacks/web.py
@@ -1,10 +1,11 @@
-from .nina import get_tts
+#from .nina import get_tts
 from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/tts', methods=['POST'])
 def tts():
-    return get_tts(request.get_json()['text'])
+#    return get_tts(request.get_json()['text'])
+    pass
 
 @app.route('/')
 def index():
